d599440b9c170178104aca90159b3f9eef44ea36
Robert Graman <rjgxfv@mail.missouri.edu>
2019-01-24 13:43:14 -0600

    Initial Commit

---
+++ Expenses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Expenses
+//
+//  Created by Robert Graman on 1/24/19.
+//  Copyright © 2019 Robert Graman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
